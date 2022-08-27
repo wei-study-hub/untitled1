edb994faefafe080b9c233bd953b32db474898b1
master <834890544@qq.com>
2022-08-27 18:45:36 +0800

    Changes

---
+++ src/main/java/com/paps/dao2.java
@@ -0,0 +1,31 @@
+package com.paps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author weiwei  master
+ * @date 2022/8/27 12 49
+ * @description
+ * @className dao
+ */
+public class dao2 {
+    //psvm
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            test();
+        }
+
+    }
+
+    public static void test() {
+        //sout
+        System.out.println(11);
+        List<String> strl = new ArrayList<String>();
+        strl.add("22");
+
+    }
+
+    ;
+
+}
---
+++ src/main/java/com/paps/service/demo1.java
@@ -0,0 +1,10 @@
+package com.paps.service;
+
+/**
+ * @author weiwei  master
+ * @date 2022/8/27 13 58
+ * @description
+ * @className demo1
+ */
+public class demo1 {
+}
